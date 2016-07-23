a215005ab880b14499b1371322f9ea02f2944bd1
Anirudh <anirudhloya93@gmail.com>
2016-07-24 02:23:31 +0530

    multiple charts

--- chart.php
+++ chart.php
@@ -1,15 +1,86 @@
 <html>
 <head>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <title>Age Tracker</title>
 </head>
 <body onLoad="filloption();">
+<?php
+$servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "csv_db";
+		//$start=$_GET["start"];
+		//$end=$_GET["end"];
+		//$chart=$_GET["chart"];
+$selected = new mysqli($servername, $username, $password, $dbname);
+if ($selected->connect_error) {
+            die("Connection failed: " . $selected->connect_error);
+        }
+//execute query
+$sql = "SELECT Age,Count(Age) As `No` FROM tbl_name GROUP BY Age"; /* WHERE YEAR(STR_TO_DATE(`Last Tracking date`, "%d-%M-%yy")) 
+BETWEEN $start AND $end";*/
+    $result = mysqli_query($selected, $sql) or die("Error in Selecting " . mysqli_error($selected));
+//fetch data
+$entry="";
+while ($row = mysqli_fetch_array($result,MYSQLI_BOTH)) {
+    $entry .= "['".$row{'Age'}."',".$row{'No'}."],";
+}
+//close the connection
+mysqli_close($selected);
+?>
 <script>
-	function selecion()
+var data="";
+var options="";
+    google.load("visualization", "1", {packages:["corechart"]});
+    google.setOnLoadCallback(drawChart);
+	    function drawChart() {
+       data = google.visualization.arrayToDataTable([
+        ['Age','No'],
+        <?php echo $entry ?>
+    ]);
+       options = {
+            title: 'Age Tracker',
+            curveType: 'function',
+            legend: { position: 'bottom' }
+        };
+		var chart1=document.getElementById("charttype").value;
+		chart1=chart1.replace(/ +/g, "");
+		var chart="";
+        if(chart1=="BarChart")
+		chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+		else if(chart1=="LineChart")
+		chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+		else if(chart1=="AreaChart")
+		chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+		else if(chart1=="PieChart")
+		chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+		else if(chart1=="ColumnChart")
+		chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    }
+
+function selection()
 {
-document.getElementById("chart_div").style.visibility="visible";
+document.getElementById("chart_div").innerHTML="";
 var start=document.getElementById("start").value;
 var end=document.getElementById("end").value;
-var chart=document.getElementById("charttype").value;
+//window.location.href = "chart.php?start=" + start + "&end=" + end+ "&chart=" + chart;
+var chart1=document.getElementById("charttype").value;
+		chart1=chart1.replace(/ +/g, "");
+		var chart="";
+        if(chart1=="BarChart")
+		chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+		else if(chart1=="LineChart")
+		chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+		else if(chart1=="AreaChart")
+		chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+		else if(chart1=="PieChart")
+		chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+		else if(chart1=="ColumnChart")
+		chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    	chart.draw(data,options);
 }
 
 </script>
@@ -19,53 +90,18 @@ Year Range:-
 <select id="end" name="end">
 </select><BR/><BR/>
 Chart Type:- <select id="charttype" name="charttype">
-<option>Line Chart</option>
 <option>Bar Chart</option>
+<option>Line Chart</option>
 <option>Pie Chart</option>
 <option>Area Chart</option>
 <option>Column Chart</option>
 </select><BR/><BR/>
-<input type="button" value="GO" onClick="selection()">
-<?php
-$servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "csv_db";
-$selected = new mysqli($servername, $username, $password, $dbname);
-if ($selected->connect_error) {
-            die("Connection failed: " . $selected->connect_error);
-        }
-//execute query
- $sql = "SELECT Age,Count(Age) As `No` FROM tbl_name GROUP BY Age";
-    $result = mysqli_query($selected, $sql) or die("Error in Selecting " . mysqli_error($connection));
-//fetch data
-$entry="";
-while ($row = mysqli_fetch_array($result,MYSQLI_BOTH)) {
-    $entry .= "['".$row{'Age'}."',".$row{'No'}."],";
-}
-//close the connection
-mysqli_close($selected);
-?>
+<input type="button" id="refresh" value="GO" onClick="selection()">
 
-<div id="chart_div" style="width: 100%; height: 500px; visibility:hidden"></div>
 
-<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<div id="chart_div" style="width: 100%; height: 500px;"></div>
+
 <script type="text/javascript">
-    google.load("visualization", "1", {packages:["corechart"]});
-    google.setOnLoadCallback(drawChart);
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-        ['Age',	'No'],
-        <?php echo $entry ?>
-    ]);
-        var options = {
-            title: 'Age Tracker',
-            curveType: 'function',
-            legend: { position: 'bottom' }
-        };
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-    }
 	
 
 function filloption()
@@ -85,6 +121,7 @@ function filloption()
 		
 		select1.appendChild(opt1);
    		select2.appendChild(opt);
+//		document.getElementById("chart_div").style.visibility="hidden";
 	}
 }
 </script>
