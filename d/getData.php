3955994811b497736b688c040b73cdf53cf7f799
gireeshst <gireeshst@yahoo.com>
2016-07-23 17:40:11 +0530

    new

---
+++ d/getData.php
@@ -0,0 +1,28 @@
+<?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "password";
+        $dbname = "DaD";
+        // Create connection
+        $connection = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+
+    //fetch table rows from mysql db
+    $sql = "select * from tbl_name";
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+    //create an array
+    $emparray = array();
+    
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+
+    echo json_encode($emparray);
+    
+    mysqli_close($connection); 
+
+?>
